8e96e752771ef60d0329ae5c7211440d23907aca
Adem Hatay <hatayadem5@gmail.com>
2024-09-21 19:18:09 +0300

    husy test

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 
-import SettingsProvider from './context/SettingsContext';
-import GameProvider from './context/GameContext';
+import SettingsProvider from "./context/SettingsContext";
+import GameProvider from "./context/GameContext";
 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Game from './components/Game';
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Game from "./components/Game";
 
 const App = () => {
-
-	return (
-		<Container className="my-4 text-center">
-			<SettingsProvider>
-				<GameProvider>
-					<Header />
-					<Game />
-					<Footer />
-				</GameProvider>
-			</SettingsProvider>
-
-
-
-		</Container>
-	);
+  return (
+    <Container className="my-4 text-center">
+      <SettingsProvider>
+        <GameProvider>
+          <Header />
+          <Game />
+          <Footer />
+        </GameProvider>
+      </SettingsProvider>
+    </Container>
+  );
 };
 
 export default App;
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,64 +1,63 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
 
-import { useSettings } from '../context/SettingsContext';
-import { useGame } from '../context/GameContext';
-import Easy from './levels/Easy';
-import Medium from './levels/Normal';
-import Hard from './levels/Hard';
+import { useSettings } from "../context/SettingsContext";
+import { useGame } from "../context/GameContext";
+import Easy from "./levels/Easy";
+import Medium from "./levels/Normal";
+import Hard from "./levels/Hard";
 
 const Game = () => {
+  const data = useSettings();
+  const gameData = useGame();
+  const { level, imageGroup } = data;
+  const {
+    cards,
+    setChoiceOne,
+    setChoiceTwo,
+    setCards,
+    setTurns,
+    characters,
+    shuffleCardsEasy,
+    shuffleCardsMedium,
+    shuffleCardsHard,
+    shuffleCardsVeryHard,
+  } = gameData;
 
+  const easy = level === "easy";
+  const easyCards = imageGroup.slice(0, 3);
 
-	const data = useSettings();
-	const gameData = useGame();
-	const { level, imageGroup } = data;
-	const { cards, setChoiceOne, setChoiceTwo, setCards, setTurns, characters,
-		shuffleCardsEasy, shuffleCardsMedium, shuffleCardsHard, shuffleCardsVeryHard } = gameData;
+  const medium = level === "normal";
+  const normalCards = characters.slice(0, 4);
 
+  const hard = level === "hard";
+  const hardCards = characters.slice(0, 5);
 
-	const easy = level === 'easy';
-	const easyCards =  imageGroup.slice(0, 3);
+  const veryHard = level === "veryHard";
+  const veryHardCards = characters.slice(0, 6);
 
-
-	const medium = level === 'normal';
-	const normalCards = characters.slice(0, 4);
-
-	const hard = level === 'hard';
-	const hardCards = characters.slice(0, 5);
-
-	const veryHard = (level === 'veryHard');
-	const veryHardCards = characters.slice(0, 6);
-
-
-	const shuffleCards = () => {
-
-		// shuffle the card level. 3x3, 4x4, 5x5, 6x6
-		if (easy) {
-			shuffleCardsEasy();
-		} else if (medium) {
-			shuffleCardsMedium();
-		} else if (hard) {
-
-			const shuffleEasyCards = [...hardCards, ...hardCards]
-				.sort(() => Math.random() - 0.5)
-				.map((card) => ({...card, id: Math.random() }))
-			setCards(shuffleEasyCards);
-		}
-
-        setChoiceOne(null);
-        setChoiceTwo(null);
-        setTurns(0);
+  const shuffleCards = () => {
+    // shuffle the card level. 3x3, 4x4, 5x5, 6x6
+    if (easy) {
+      shuffleCardsEasy();
+    } else if (medium) {
+      shuffleCardsMedium();
+    } else if (hard) {
+      const shuffleEasyCards = [...hardCards, ...hardCards]
+        .sort(() => Math.random() - 0.5)
+        .map((card) => ({ ...card, id: Math.random() }));
+      setCards(shuffleEasyCards);
     }
 
-	useEffect(() => {
-        shuffleCards();
-    }, [])
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(0);
+  };
 
-	return <>
-			{
-				easy ? <Easy /> : medium ? <Medium /> : hard ? <Hard /> : null
-			}
-	</>
-}
+  useEffect(() => {
+    shuffleCards();
+  }, []);
 
-export default Game
+  return <>{easy ? <Easy /> : medium ? <Medium /> : hard ? <Hard /> : null}</>;
+};
+
+export default Game;
